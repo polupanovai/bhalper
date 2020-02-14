82abb0357ab0a8b7c9cf6dae0d55bb51ed3ed4c0
root <root@vm-5858ab06.netangels.ru>
2020-02-14 11:53:50 +0000

    commit blyat'

---
+++ src/BHalper.php
@@ -0,0 +1,17 @@
+<?php
+namespace Polupaovai\Tools\B24Halper;
+
+class BHalper
+{
+    public function __construct()
+    {
+
+    }
+
+    /* Инициализация */
+    public function init()
+    {
+        return 'hallow wolrd 2';
+    }
+
+}
